c7ae7b0040ebcf8ab69c0ce876cfa323bda877da
amarigom <andreamarigomez@gmail.com>
2026-02-21 17:47:57 -0300

    Limpieza: dejando de rastrear carpeta .next

--- .next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/8b3b6_939ff96e._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__e6a4d965._.js")
-R.m("[project]/Desktop/ANDREA/PROGRAMACION/ROLLER/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/Desktop/ANDREA/PROGRAMACION/ROLLER/node_modules/next/document.js [ssr] (ecmascript)").exports
--- .next/dev/static/chunks/Desktop_ANDREA_PROGRAMACION_ROLLER_app_page_tsx_37d5c589._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Desktop_ANDREA_PROGRAMACION_ROLLER_2fcc1ecb._.js"
-],
-    source: "dynamic"
-});
