05cbc8fc556adf1e6370de92a2ad5f7f4c469d6e
amarigom <andreamarigomez@gmail.com>
2026-02-20 22:51:39 -0300

    arreglando color de checkbox

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -14,9 +14,13 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        'peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
+    "peer h-4 w-4 shrink-0 rounded-sm border border-[--color-primary] ring-offset-background",
+    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[--color-primary] focus-visible:ring-offset-2",
+    "disabled:cursor-not-allowed disabled:opacity-50",
+    // Aquí es donde le das el color cuando está marcado
+    "data-[state=checked]:bg-[--color-primary] data-[state=checked]:text-[#0a0a0a]",
+    className
+  )}
       {...props}
     >
       <CheckboxPrimitive.Indicator
