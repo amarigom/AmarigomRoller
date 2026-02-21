6a96659429b5056874d58a4ec59d3385f899082d
amarigom <andreamarigomez@gmail.com>
2026-02-20 22:28:38 -0300

    probando cambios en toast

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -28,10 +28,11 @@ const toastVariants = cva(
   'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full',
   {
     variants: {
-      variant: {
-        default: 'border bg-background text-foreground',
+      variant: {// Fondo oscuro (Surface), Texto Blanco Hueso, Borde Dorado
+        default: 'border-[--color-primary] bg-[--color-surface] text-[--color-text-primary]',
+        // Variante de error usando tu color-error desaturado
         destructive:
-          'destructive group border-destructive bg-destructive text-destructive-foreground',
+          'border-[--color-error] bg-[--color-surface] text-[--color-text-primary] group',
       },
     },
     defaultVariants: {
@@ -77,7 +78,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      'absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600',
+      'absolute right-2 top-2 rounded-md p-1 text-[--color-text-muted] opacity-0 transition-opacity hover:text-[--color-primary] focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100',
       className,
     )}
     toast-close=""
@@ -94,7 +95,7 @@ const ToastTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Title
     ref={ref}
-    className={cn('text-sm font-semibold', className)}
+    className={cn('text-sm font-semibold text-[--color-text-primary]', className)}
     {...props}
   />
 ))
@@ -106,7 +107,7 @@ const ToastDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Description
     ref={ref}
-    className={cn('text-sm opacity-90', className)}
+    className={cn('text-sm text-[--color-text-secondary] opacity-90', className)}
     {...props}
   />
 ))
