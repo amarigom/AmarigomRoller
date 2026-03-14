de4c769933784c6c06300b6fe6c18bc8388fede0
amarigom <andreamarigomez@gmail.com>
2026-03-14 16:11:20 -0300

    app=app

--- backend/app.py
+++ backend/app.py
@@ -54,7 +54,7 @@ with app.app_context():
 @app.context_processor
 def inject_language():
     return dict(lang=session.get('language', 'es'))
-
+app = app
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
