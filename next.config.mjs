e7cc195f56e347d5acaa9013893eaf3cf50e1dcf
amarigom <andreamarigomez@gmail.com>
2026-03-14 21:35:57 -0300

    Forzar legacy-peer-deps para evitar conflicto de vaul

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://amarigom-roller-e02bak03n-andreas-projects-71d69b69.vercel.app/api/:path*',
+        destination: 'https://amarigom-roller-n8ptnb6sn-andreas-projects-71d69b69.vercel.app/api/:path*',
       },
     ];
   },
