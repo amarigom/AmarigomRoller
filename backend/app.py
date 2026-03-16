51bb0f2dcd6c4ea961ee98cdfb5389130aa7d6d6
amarigom <andreamarigomez@gmail.com>
2026-03-16 18:49:51 -0300

    config BD

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,7 @@ else:
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "amarigom-secret-2026")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 # ---------------------------------
 
 CORS(app)
